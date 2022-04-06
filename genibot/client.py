2b2d7f72b2a36cb39d0e37c944f9ff967bfb71e4
Diego Rodriguez <diego.vincent.rodriguez@gmail.com>
2022-04-05 23:49:29 -0400

    feat: bot can send new generation

--- genibot/client.py
+++ genibot/client.py
@@ -4,7 +4,17 @@ import abc
 class TwitterClient(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def get_latest_mentions(self):
-        """Return the latest Twitter mentions to the bot."""
+        """Return the latest Twitter mentions to the bot that the bot has not
+replied to yet."""
+
+    def send_reply(self, in_reply_to_tweet_id, generation_as_bytes):
+        """Sends a Twitter reply.  Needs tweet ID and a generation."""
+
+
+class GenerationRepository(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def find_by_id(self, generation_id):
+        """Returns an image generation based on the ID of the generation."""
 
 
 class Bot:
@@ -16,8 +26,37 @@ dictionary containing the following:
 
         """
         self.twitter_client = config["twitter_client"]
+        self.generation_repository = config["storage_client"]
 
     def check_for_new_tweets(self):
         latest_mentions = self.twitter_client.get_latest_mentions()
 
         return latest_mentions
+
+    def send_generation(self, generation_params):
+        """Send a new generation to a user based on the generation
+configuration."""
+
+        # Download generation from storage (e.g. S3)
+        generation_id = generation_params["generation_id"]
+        generation_as_bytes = self.generation_repository.find_by_id(
+            generation_id,
+        )
+
+        # Send tweet to user with generation attached
+        tweet_data = generation_params["original_tweet_data"]
+        twitter_username = tweet_data["username"]
+        tweet_to_reply_to_id = tweet_data["tweet_id"]
+        sent_tweet_data = self.twitter_client.send_reply(
+            in_reply_to_tweet_id=tweet_to_reply_to_id,
+            generation_as_bytes=generation_as_bytes,
+        )
+
+        # Return the ID of the generation sent and the ID of the reply
+        # tweet.
+        return {
+            "generation_id": generation_id,
+            "sent_tweet_id": sent_tweet_data["tweet_id"]
+        }
+            
+
--- tests/test_initialization.py
+++ tests/test_initialization.py
@@ -1,13 +1,39 @@
 import genibot
-from genibot.client import TwitterClient
+from genibot.client import TwitterClient, GenerationRepository
 
 @TwitterClient.register
 class MockTwitterClient:
     def __init__(self, latest_mentions=[]):
         self.latest_mentions = latest_mentions
 
-    def get_latest_mentions(self):
+    def get_latest_mentions(self, most_recent_buffer_size=10):
         return self.latest_mentions
+
+    def send_reply(self, in_reply_to_tweet_id, generation_as_bytes):
+        assert generation_as_bytes is not None
+
+        # Create fake tweet
+        fake_tweet_id = 0
+
+        # Return information about that tweet
+        return {
+            "tweet_id": fake_tweet_id,
+        }
+
+
+@GenerationRepository.register
+class MockGenerationRepository:
+    def __init__(self, generations={}):
+        self.generations = generations
+
+    def find_by_id(self, generation_id):
+        try:
+            generation = self.generations[generation_id]
+        except KeyError:
+            print(f"Generation with ID {generation_id} not found")
+            raise 
+
+        return generation
 
 
 def test_genibot_module_is_imported_correctly():
@@ -16,7 +42,11 @@ def test_genibot_module_is_imported_correctly():
 
 def test_genibot_initialization():
     empty_twitter_client = MockTwitterClient()
-    config = {"twitter_client": empty_twitter_client}
+    empty_generation_repository = MockGenerationRepository()
+    config = {
+        "twitter_client": empty_twitter_client,
+        "storage_client": empty_generation_repository,
+    }
 
     bot = genibot.init(config)
 
@@ -27,7 +57,11 @@ def test_bot_checks_if_there_is_a_new_tweet():
     # Setup
     latest_mentions = [{"prompt": "a beautiful face"}]
     twitter_client = MockTwitterClient(latest_mentions)
-    config = {"twitter_client": twitter_client}
+    generation_repository = MockGenerationRepository()
+    config = {
+        "twitter_client": twitter_client,
+        "storage_client": generation_repository,
+    }
 
     bot = genibot.init(config)
 
@@ -36,3 +70,41 @@ def test_bot_checks_if_there_is_a_new_tweet():
 
     # Assertions
     assert len(new_tweets) > 0
+
+
+def test_bot_sends_a_new_generation():
+    stored_generations = {
+        "GENERATION_ID": b'generation_bytes',
+    }
+
+    config = {
+        "twitter_client": MockTwitterClient(),
+        "storage_client": MockGenerationRepository(stored_generations),
+    }
+    bot = genibot.init(config)
+
+    generation_notification_msg = {
+        "generation_id": "GENERATION_ID",
+        "original_tweet_data": {
+            "username": "@iamdiegovincent",
+            "tweet_id": 123456,
+            "tweet": "a beautiful corgi in pixel art style",
+        },
+    }
+
+    # when a generation has been stored, a notification comes in that
+    # says that the generation is ready to be downloaded.  Hence, is
+    # ready for our bot to download the generation and send a Twitter
+    # reply to the user who asked for the generation.  All of the
+    # necessary information can be found in the notification message.
+    generation_result = bot.send_generation(
+        generation_params=generation_notification_msg,
+    )
+
+    # After the generation has been downloaded and sent, the bot will
+    # have tweeted the generation and it outputs the unique generation
+    # ID as well as the unique tweet ID.
+    assert generation_result == {
+        "generation_id": "GENERATION_ID",
+        "sent_tweet_id": 0,
+    }
